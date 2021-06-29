9e4355bbe979c20d1b0bce0de163f4e386c74124
Enea Muskaj <eneamuskaj@gmail.com>
2021-06-29 16:04:45 -0500

    added more cards and styling

--- src/components/cardDisplay.js
+++ src/components/cardDisplay.js
@@ -1,22 +1,22 @@
 const CardDisplay = (props) => {
   let items = []
-  for (let i = 0; i < 9; i++) {
+  for (let i = 1; i < 66; i++) {
     items.push(
       <img
         className="imagesClass"
-        src={'images/' + i + '.png'}
+        src={'images/1 (' + i + ').png'} //1 (1)
         alt="card"
-        style={{ width: 25 + '%' }}
+        style={{ width: 15 + '%' }}
         onClick={props.handleClick}
         id={i}
       />
     )
   }
   let randomArray = []
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 12; i++) {
     randomArray.push(items[Math.floor(Math.random() * items.length)])
   }
-  return <div>{items}</div>
+  return <div>{randomArray}</div>
 }
 
 export default CardDisplay
